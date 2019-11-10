1aba45722f3dcd6bf5b405f91e4665d4ab1c5658
“lixuan” <994047477@qq.com>
2019-11-10 15:19:26 +0800

    finish code counter and save to all_word.pkl

--- code_parse.py
+++ code_parse.py
@@ -7,10 +7,11 @@
 @desc: 
 '''
 
-import plyj.parser # pip install plyj
+import plyj.parser  # pip install plyj
 import glob
-
+import csv
 from ply import lex, yacc
+import pickle
 
 
 class Parser(object):
@@ -21,10 +22,11 @@ class Parser(object):
 
     def tokenize_string(self, code):
         self.lexer.input(code)
-        lis =[]
+        lis = []
         for token in self.lexer:
             lis.append(token)
         return lis
+
     def tokenize_file(self, _file):
         if type(_file) == str:
             _file = open(_file)
@@ -52,16 +54,29 @@ class Parser(object):
         return self.parse_string(content, debug=debug)
 
 
-parser =Parser()
-lis = glob.glob('../ASE_javacode/*')
-print(lis[0])
+if __name__ == '__main__':
+    # 完成代码统计任务 ，并存到文件中
+    parser = Parser()
+    lis = glob.glob('../ASE_javacode/*')
+    di = dict()
+    for i in range(len(lis)):
+        print(f'{i}/{len(lis)}  {lis[i]}')
+        try :
+            res = parser.tokenize_file(lis[i])
+        except Exception :
+            continue
+        for i in range(len(res)):
+            # print(res[i],res[i].type, res[i].value, res[i].lineno, res[i].lexpos ) # NUM  和 STRING_LITERAL
+            if res[i].type == 'NUM':
+                # print(res[i], res[i].type, res[i].value, res[i].lineno, res[i].lexpos)  # NUM  和 STRING_LITERAL
 
-res = parser.tokenize_file(lis[0])
-for i in range(len(res)):
+                di['<num>'] = di.get('<num>',0)+1
+            elif res[i].type == 'STRING_LITERAL':
+                di['<str>'] = di.get('<str>',0)+1
+            else:
+                di[res[i].value]=di.get(res[i].value,0)+1
+        print('dict len', len(di))
 
-    # print(res[i],res[i].type, res[i].value, res[i].lineno, res[i].lexpos ) # NUM  和 STRING_LITERAL
-    # if res[i].type=='NUM' or res[i].type=='STRING_LITERAL' :
-        print(res[i], res[i].type, res[i].value, res[i].lineno, res[i].lexpos)  # NUM  和 STRING_LITERAL
-
-
+    with open('all_word.pkl', 'wb')as f:
+        pickle.dump(di, f)
 
